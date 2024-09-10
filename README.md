b0065031953523f9b6f488c6d5adb4f0a394ddfb
Pramuditha <pramudithakavinda01@gmail.com>
2024-09-10 20:30:57 +0530

    Authentication

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ A Person Who Loves Mountains
 
 Last Updated 9/9/2024
 Last Time 42:50 Authentication
+50.30
---
+++ api/index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.post('/register',(req,res) => {
+    const {username,password} = req.body;
+    res.json({requestData:{username,password}});
+});
+
+app.listen(4000);
